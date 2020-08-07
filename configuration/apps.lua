7ebaacba14dd7ab747b411473c4e7a864406030a
Matt Spurrier <matthew@senseict.com.au>
2020-08-08 01:14:49 +0800

    update for my usecase

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -8,19 +8,18 @@ local rofi_command = 'env /usr/bin/rofi -dpi ' .. get_dpi() .. ' -width ' .. wit
 return {
   -- List of apps to start by default on some actions
   default = {
-    terminal = 'env x-terminal-emulator -e /usr/bin/env TERM=xterm-256color byobu',
+    terminal = '/home/matt/.cargo/bin/alacritty',
     rofi = rofi_command,
     lock = 'i3lock-fancy',
-    quake = 'alacritty',
     screenshot = '~/.config/awesome/configuration/utils/screenshot -m',
-    region_screenshot = '~/.config/awesome/configuration/utils/screenshot -r',
+    region_sxcreenshot = '~/.config/awesome/configuration/utils/screenshot -r',
     delayed_screenshot = '~/.config/awesome/configuration/utils/screenshot --delayed -r',
-    
+
     -- Editing these also edits the default program
     -- associated with each tag/workspace
-    browser = 'env vivaldi',
-    editor = 'code', -- gui text editor
-    social = 'env discord',
+    browser = 'env brave-browser-nightly',
+    editor = 'subl', -- gui text editor
+    social = 'flatpak run com.discordapp.Discord',
     game = rofi_command,
     files = 'nautilus',
     music = rofi_command
@@ -29,15 +28,15 @@ return {
   run_on_start_up = {
     'compton --config ' .. filesystem.get_configuration_dir() .. '/configuration/compton.conf',
     'nm-applet --indicator', -- wifi
-    --'blueberry-tray', -- Bluetooth tray icon
-    --'xfce4-power-manager', -- Power manager
+    'blueberry-tray', -- Bluetooth tray icon
+    'xfce4-power-manager', -- Power manager
     'ibus-daemon --xim --daemonize', -- Ibus daemon for keyboard
-    'scream-start', -- scream audio sink
+    'pulseaudio', -- pulse audio
     'numlockx on', -- enable numlock
     '/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
     --'/usr/lib/x86_64-linux-gnu/libexec/polkit-kde-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
-    'blueman-tray', -- bluetooth tray
-    'geary --hidden', -- Email client
+    --'blueman-tray', -- bluetooth tray
+    --'geary --hidden', -- Email client
     -- Add applications that need to be killed between reloads
     -- to avoid multipled instances, inside the awspawn script
     '~/.config/awesome/configuration/awspawn' -- Spawn "dirty" apps that can linger between sessions
